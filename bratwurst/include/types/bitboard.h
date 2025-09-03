20fa59fbd3692de74fc6c85ab433be62ae690e50
ConstantinTopcu <const.topcu@gmail.com>
2025-09-03 14:42:01 +0200

    Add full bitboard with masks, pop/lsb/msb, popcnt, RNG, and unit tests

---
+++ bratwurst/include/types/bitboard.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "core.h"
+#include "square.h"
+
+#include <random>
+#ifdef COMPILER_MSVC
+#include <intrin.h>
+#endif
+
+namespace Bratwurst
+{
+
+using Bitboard = uint64;
+
+constexpr Bitboard squareMask(Square s) noexcept
+{
+	ASSERT(isValid(s));
+	return 1ULL << s;
+}
+
+constexpr Bitboard rankMask(Rank r) noexcept
+{
+	ASSERT(isValid(r));
+	return 0xFFULL << (r * FileNum);
+}
+
+constexpr Bitboard fileMask(File f) noexcept
+{
+	ASSERT(isValid(f));
+	return 0x0101010101010101ULL << f;
+}
+
+inline Bitboard randomBitboard() noexcept
+{
+	static std::random_device rd;
+	static std::mt19937_64 gen(rd());
+	static std::uniform_int_distribution<Bitboard> distrib;
+	return distrib(gen);
+}
+
+inline Square lsb(Bitboard mask) noexcept
+{
+	ASSERT(mask != 0ULL);
+
+#if defined(COMPILER_MSVC)
+	unsigned long lsb;
+	_BitScanForward64(&lsb, mask);
+	return static_cast<Square>(lsb);
+#elif defined(COMPILER_GCC) || defined(COMPILER_CLANG)
+	int i = _builtin_ctzll(mask);
+	return static_cast<Square>(i);
+#endif
+}
+
+inline Square msb(Bitboard mask) noexcept
+{
+	ASSERT(mask != 0ULL);
+
+#if defined(COMPILER_MSVC)
+	unsigned long msb;
+	_BitScanReverse64(&msb, mask);
+	return static_cast<Square>(msb);
+#elif defined(COMPILER_GCC) || defined(COMPILER_CLANG)
+	return static_cast<Square>(H8 - __builtin_clzll(mask));
+#endif
+}
+
+inline Square popLsb(Bitboard& mask) noexcept
+{
+	ASSERT(mask != 0ULL);
+	const Square square = lsb(mask);
+	mask &= mask - 1;
+	return square;
+}
+
+inline Square popMsb(Bitboard& mask) noexcept
+{
+	ASSERT(mask != 0ULL);
+	const Square square = msb(mask);
+	mask &= mask ^ squareMask(square);
+	return square;
+}
+
+inline int popCnt(Bitboard mask) noexcept
+{
+#if defined(COMPILER_MSVC)
+	return static_cast<int>(__popcnt64(mask));
+#elif defined(COMPILER_GCC) || defined(COMPILER_CLANG)
+	return static_cast<int>(__builtin_popcountll(mask));
+#endif
+}
+
+}
---
+++ tests/bitboard_test.cpp
@@ -0,0 +1,57 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_all.hpp>
+
+#include <types/bitboard.h>
+
+using namespace Bratwurst;
+
+TEST_CASE("Bitboard squareMask") 
+{
+    REQUIRE(squareMask(A1) == 1ULL << A1);
+    REQUIRE(squareMask(H8) == 1ULL << H8);
+}
+
+TEST_CASE("Bitboard rankMask") 
+{
+    REQUIRE(rankMask(Rank1) == 0xFFULL);
+    REQUIRE(rankMask(Rank8) == (0xFFULL << (7 * FileNum)));
+}
+
+TEST_CASE("Bitboard fileMask") 
+{
+    REQUIRE(fileMask(FileA) == 0x0101010101010101ULL);
+    REQUIRE(fileMask(FileH) == (0x0101010101010101ULL << 7));
+}
+
+TEST_CASE("Bitboard popCnt") 
+{
+    REQUIRE(popCnt(0ULL) == 0);
+    REQUIRE(popCnt(0xFULL) == 4);
+    REQUIRE(popCnt(0xFFFFFFFFFFFFFFFFULL) == 64);
+}
+
+TEST_CASE("Bitboard lsb/msb") 
+{
+    Bitboard bb = squareMask(C3) | squareMask(H8);
+    REQUIRE(lsb(bb) == C3);
+    REQUIRE(msb(bb) == H8);
+}
+
+TEST_CASE("Bitboard popLsb/popMsb") 
+{
+    Bitboard bb = squareMask(B2) | squareMask(G7);
+
+    Square s1 = popLsb(bb);
+    REQUIRE((s1 == B2 || s1 == G7));
+    REQUIRE(popCnt(bb) == 1);
+
+    Square s2 = popMsb(bb);
+    REQUIRE((s2 == B2 || s2 == G7));
+    REQUIRE(bb == 0ULL);
+}
+
+TEST_CASE("Bitboard randomBitboard") {
+    Bitboard b1 = randomBitboard();
+    Bitboard b2 = randomBitboard();
+    REQUIRE(b1 != b2); // very unlikely to fail
+}
